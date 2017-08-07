0a5fa01c14c7379c42cdf0308682e8d4bc03e709
unknown <panosziogas@hotmail.com>
2017-08-07 16:47:40 +0300

    added shell sort

--- SortingAlgorithmsComparator/src/main/java/gr/panosziogas/sortingalgorithmscomparator/algorithms/CountingSort.java
+++ SortingAlgorithmsComparator/src/main/java/gr/panosziogas/sortingalgorithmscomparator/algorithms/CountingSort.java
@@ -12,36 +12,26 @@ public class CountingSort implements AlgorithmsInterface {
     public Integer[] sortArray(Integer[] unsortedArray) {
         Integer n = unsortedArray.length;
         int min = 0, max = 0;
-
         for (int i = 1; i < n; i++) {
-
             if (unsortedArray[i] > max) {
                 max = unsortedArray[i];
             }
-
             if (unsortedArray[i] < min) {
                 min = unsortedArray[i];
             }
-
         }
-
         int range = max - min + 1;
-
         int[] count = new int[range];
-
         //counts frequencies for each element
         for (int i = 0; i < n; i++) {
             count[unsortedArray[i] - min]++;
         }
-
         // getting positions in final array
         for (int i = 1; i < range; i++) {
             count[i] += count[i - 1];
         }
-
         //copy to output array, preserving order of inputs with equal keys
         int j = 0;
-
         for (int i = 0; i < range; i++) {
             while (j < count[i]) {
                 unsortedArray[j++] = i + min;
